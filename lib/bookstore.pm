2210a5c6e0915b31530738d8ddd1452948e933d1
Sean Zellmer <sean@lejeunerenard.com>
2013-02-02 01:23:24 -0600

    Added route and template

--- lib/bookstore.pm
+++ lib/bookstore.pm
@@ -7,4 +7,15 @@ get '/' => sub {
     template 'index';
 };
 
+get '/search' => sub {
+    my $query = params->{query};
+    my @results = ();
+    if (length $query) {
+        @results = _perform_search($query);
+    }
+    template 'search', { query => $query,
+                         results => \@results,
+                       };
+};
+
 true;
